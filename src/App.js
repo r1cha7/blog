36877f2100ebf70952f4d37f55c7e0132c499fb7
r1cha7 <21ecuos101@ddu.ac.in>
2024-03-11 18:51:22 +0530

    home page created

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+//import Base from './pages/Base';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import About from './pages/About';
+import Services from './pages/Services';
+import PostPage from "./pages/PostPage";
+import Categories from "./pages/Categories";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='services' element={ <Services />} />
+        <Route path='/about' element={<About />} />
+        <Route path="/posts/:postId" element={<PostPage />} />
+        <Route path="/categories/:categoryId" element={<Categories />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
